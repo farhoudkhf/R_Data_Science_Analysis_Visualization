c2a2491a6d1b4e5e41fbd2e1c5c333f0f27648df
Farhoud Khoshnoud <farhoudkhf@gmail.com>
2025-06-08 11:45:58 -0700

    c11-ch3-Data_Visualization

--- R for Data Science_ Analysis and Visualization/R_ExerciseFiles/code/ch3-Data_Visualization/03_07_DoNotUse-ClusterCharts.R
+++ R for Data Science_ Analysis and Visualization/R_ExerciseFiles/code/ch3-Data_Visualization/03_07_DoNotUse-ClusterCharts.R
@@ -6,55 +6,29 @@
 # RStudio will prompt you to download any packages that
 # aren't already installed.
 
-install.packages("ggdendro") # Alternative visualization
-
 # Load packages
 library(tidyverse)  # Loads the `tidyverse` collection
 library(readxl)     # Reads CSV and Excel files
-library(ggdendro)   # loads alternative visulization
-
 
 # LOAD DATA ################################################
 
 # Select state codes and search data
 df <- read_csv("data/state_trends.csv") |>
-  select(state_code, artificial_intelligence:hockey) |>
-  drop_na() # remove cases with missing values
-
-# Standardize and Prepare data #############################################
-
-# Standardize Quantetative Variables 
-df_scaled <- df |>
-  select(-state_code) |>  # remove state code from scaling
-  scale()                 # standardize variables
-
-# Assign state codes as row name
-rownames(df_scaled) <- df$state_code
+  select(state_code, artificial_intelligence:hockey)
 
 # ANALYZE DATA #############################################
 
 # Calculate clusters using hclust(), an agglomerative method
-hc <- df_scaled |>  # Get data
+hc <- df |>  # Get data
   dist() |>  # Compute distance/dissimilarity matrix
   hclust()   # Compute hierarchical clusters
 
 # Plot dendrogram
-hc |> plot(
-  labels = rownames(df_scaled), # df$state_code
-  main = "Cluster Dendrogram of States"
-  )
+hc |> plot(labels = df$state_code)
 
 # Draw boxes around clusters
 hc |> rect.hclust(k = 2, border = "gray80")  # 2 boxes
 hc |> rect.hclust(k = 3, border = 2:4)       # 3 boxes
-
-
-# Alternative Visualization from ‘ggdendro’
-hc |> ggdendrogram(
-  rotate = TRUE,         # Rotate to Horizontal
-  theme_dendro = FALSE  # Turns off
-)
-
 
 # CLEAN UP #################################################
 
